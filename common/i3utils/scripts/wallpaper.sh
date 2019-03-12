bf303ac70e28895513d5e344580c319d37036c1c
Sevan Murriguian <murrig_s@epita.fr>
2019-03-13 00:26:18 +0100

    upd : random wallpaper script

--- common/i3utils/scripts/wallpaper.sh
+++ common/i3utils/scripts/wallpaper.sh
@@ -1,12 +1,25 @@
 #!/bin/sh
 
+# find . -type f | grep -E "([^\\s]+(jpg|png|bmp))"
+# find . -type f -exec file {} \; | awk -F: '{if ($2 ~/image/) print $1}'
+
+
+rand ()
+{
+  declare -a array=("$@")
+  r=$((RANDOM % ${#array[@]}))
+  printf "%s\n" "${array[$r]}"
+}
+
 if [ ! -z "$1" ]; then
     dir="$1"
 elif [ ! -z "$WALLPAPERS_PATH" ]; then
     dir="$WALLPAPERS_PATH"
 else
-    dir="~/.config/wallpapers"
+    dir="$HOME/.config/wallpapers"
 fi
 
-files=$(ls $dir)
-printf "%s\n" "${files[RANDOM % ${#files[@]}]}"
+img="$(find -L "$dir" -type f | grep -E "([^\\s]+(jpg|png|bmp))")"
+wall="$(rand $img)"
+
+feh --bg-fill $wall

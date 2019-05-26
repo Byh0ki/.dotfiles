f0228091db13b6bd015002fcf4c8c73a1d02accf
Sevan Murriguian <murrig_s@epita.fr>
2019-05-27 01:34:49 +0200

    install: minor changes to optimize the code

--- install.sh
+++ install.sh
@@ -18,6 +18,11 @@ dotfiles_path="$( cd -P "$( dirname "$SOURCE" )" >/dev/null 2>&1 && pwd )"
 dot_list_home=".profile .bash_profile .zprofile .aliases .bashrc .gitconfig .gitignore .gtkrc-2.0 .vimrc .xinitrc .Xresources .zshCustom .zshrc"
 dot_list_conf="dunst compton.conf gtk-3.0 i3 i3utils i3status neofetch wallpapers"
 
+# Extra directories
+include_extra_home=""
+include_extra_conf=""
+exclude_extra=""
+
 # Internal script vars
 bak=".bak"
 clean_bak="$HOME/.clean"
@@ -27,7 +32,7 @@ dst="$HOME"
 
 usage()
 {
-    echo -e "./$(basename $0)   [-h] [-s] [-b backup dir] [-e extra_dir] [-d dst_dir]"
+    echo -e "./$(basename "$0")   [-h] [-s] [-b backup dir] [-e extra_dir] [-d dst_dir]"
     echo
     echo -e "               [-h] display this help message"
     echo -e "               [-s] activate the hook for suspend /!\\ root access needed /!\\"
@@ -92,7 +97,7 @@ clean_dots()
     mkdir -p "$1"
     clean_list "$dst" "$dot_list_home" "$1"
     clean_list "$dst/.config" "$dot_list_conf" "$1"
-    if [[ ! -z "$include_extra_home" || ! -z "$include_extra_conf" ]]; then
+    if [[ -n "$include_extra_home" || -n "$include_extra_conf" ]]; then
         echo "Cleaning files from extra dir..."
         clean_list "$dst" "$include_extra_home" "$1"
         clean_list "$dst/.config" "$include_extra_conf" "$1"
@@ -100,7 +105,7 @@ clean_dots()
     if [ "$(ls -A "$1" | wc -l)" -ne 0 ]; then
         echo "This are the list of files that will be deleted :"
         ls --color=auto -A "$1"
-        # Ask the user if he is sure to delete the file
+        # Ask the user if he is sure to delete the files
         read -p "Are you sure? [Y|n] " -r
         if [[ "$REPLY" =~ ^[Yy]$ ]] || [[ -z $REPLY ]]; then
             echo "rm -rf' $1"
@@ -118,9 +123,9 @@ create_symlinks()
     # $3 : backup path
     # $4 : pick file from the extra folder
     for elt in $2; do
-        if [ ! $(contain "$exclude_extra" "$elt") ]; then
+        if [ ! "$(contain "$exclude_extra" "$elt")" ]; then
             backup "$1" "$elt" "$3"                         # Backup
-            if [ ! -z "$4" ] && [[ -f "$dotfiles_path/extra/$4/$elt" || -d "$dotfiles_path/extra/$4/$elt" ]]; then
+            if [ -n "$4" ] && [[ -f "$dotfiles_path/extra/$4/$elt" || -d "$dotfiles_path/extra/$4/$elt" ]]; then
                 elt_path="$dotfiles_path/extra/$4/$elt"
             else
                 elt_path="$dotfiles_path/common/$elt"
@@ -136,9 +141,9 @@ copy_sys_dot()
     # $1 : dst dir
     # $2 : dotfile
     # $3 : pick file from the extra folder
-    if [ ! $(contain "$exclude_extra" "$2") ]; then
+    if [ ! "$(contain "$exclude_extra" "$2")" ]; then
         backup_sys "$1" "$2" "$1"                     # Backup
-        if [ ! -z "$3" ] && [[ -f "extra/$3/$2" || -d "extra/$3/$2" ]]; then
+        if [ -n "$3" ] && [[ -f "extra/$3/$2" || -d "extra/$3/$2" ]]; then
             elt_path="$dotfiles_path/extra/$3/$2"
         else
             elt_path="$dotfiles_path/common/$2"
@@ -168,17 +173,18 @@ while getopts hb:e:d:sc option; do
     esac
 done
 
-if [ ! -z "$extra" ]; then
+if [ -n "$extra" ]; then
     if [ ! -d "$dotfiles_path/extra/$extra" ]; then
         echo "This extra directory doesn't exist."
         exit 2
     fi
     if [ -f "$dotfiles_path/extra/$extra/.extra_vars" ]; then
-        source $dotfiles_path/extra/$extra/.extra_vars
+        # shellcheck source=/dev/null
+        source "$dotfiles_path/extra/$extra/.extra_vars"
     fi
 fi
 
-if [ ! -z $clean_hook ]; then
+if [ -n "$clean_hook" ]; then
     clean_dots "$clean_bak"
     exit 0
 fi
@@ -188,15 +194,15 @@ create_dirs "$dst"
 echo "Creating symlinks..."
 create_symlinks "$dst" "$dot_list_home" "$dst/$bak" "$extra"
 create_symlinks "$dst/.config" "$dot_list_conf" "$dst/$bak" "$extra"
-if [[ ! -z "$include_extra_home" || ! -z "$include_extra_conf" ]]; then
+if [[ -n "$include_extra_home" || -n "$include_extra_conf" ]]; then
     echo "Creating symlink for extra dir..."
     create_symlinks "$dst" "$include_extra_home" "$dst/$bak" "$extra"
     create_symlinks "$dst/.config" "$include_extra_conf" "$dst/$bak" "$extra"
 fi
 
-if [ ! -z $sleep_hook ]; then
+if [ -n "$sleep_hook" ]; then
     copy_sys_dot "/etc/systemd/system" "suspend@.service" "$extra"
-    sudo systemctl enable suspend@$USER.service
+    sudo systemctl enable suspend@"$USER".service
 fi
 
 exit 0
